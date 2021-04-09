0f01a2cf16ee7569a9876bbf75f70d99ab94ab04
waterfogSW <waterfog9580@gmail.com>
2021-04-09 22:14:11 +0900

    update

--- prod_cons.c
+++ prod_cons.c
@@ -2,96 +2,119 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define MAX 10
+#define LINE_SIZE 10
 
-char buffer[MAX];
-int fill_ptr = 0;
-int use_ptr = 0;
-int count = 0;
-int loops = 3;
+char order[100] = {'D','D','D','D','D','A','B','C','A','C','E','E','E','E','E','A','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B'};
 
-pthread_cond_t empty, fill;
+char buffer[LINE_SIZE];
+int rear = 0;
+int front = 0;
+int balance = 0;
+
+int total = 100;
+int pnum = 0;
+int cnum = 0;
+
+int info[5] = {30,30,30,5,5};
+
+pthread_cond_t empty;
+pthread_cond_t fill;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-typedef struct Enqueue_arg {
-	char            _value;     // Push value of queue
-}EA;
-
-typedef struct Dequeue_arg {
-    int             volume;
-	char            *_value;     // Pop value of queue
-}DA;
-
 void put(char value) {
-    buffer[fill_ptr] = value;
-    fill_ptr = (fill_ptr + 1) % MAX;
-    count++;
+    buffer[rear] = value;
+    rear = (rear + 1) % LINE_SIZE;
+    pnum++;
+    balance++;
 }
 
 void get(char* value) {
-    char tmp = buffer[use_ptr];
-    use_ptr = (use_ptr + 1) % MAX;
-    count--;
+    char tmp = buffer[front];
+    front = (front + 1) % LINE_SIZE;
+    balance--;
+    cnum++;
     *value = tmp;
 }
 
-void *producer(void *input) {
-    char *car = (char*)input;
-    for (int i = 0; i < 90; i++) {
+void *c_prod(void *input) {
+    for (int i = 0; i < total; i++) {
         pthread_mutex_lock(&mutex);
-        while (count == MAX)
+        while (balance == LINE_SIZE)
             pthread_cond_wait(&empty, &mutex);
-        put(car[i]);
+        put(order[i]);
         pthread_cond_broadcast(&fill);
         pthread_mutex_unlock(&mutex);
     }
-    while(count > 0) {
+    while(balance > 0) {
         pthread_cond_broadcast(&fill);
     }
     return NULL;
 }
 
-void *consumer_A(void *multi_arg) {
-    DA *arg = (DA *)multi_arg;
-    char *value  = arg->_value;
-    for (int i = 0; i < arg->volume; i++) {
+void *c_consA(void *multi_arg) {
+    char value;
+    for (int i = 0; i < info[0]; i++) {
         pthread_mutex_lock(&mutex);
-        while (count == 0 || buffer[use_ptr] != 'A')
+        while (balance == 0 || buffer[front] != 'A')
             pthread_cond_wait(&fill, &mutex);
-        get(value);
-        printf("A: %c \n", *value);
+        get(&value);
+        printf("A: %c \n", value);
         pthread_cond_signal(&empty);
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
-void *consumer_B(void *multi_arg) {
-    DA *arg = (DA *)multi_arg;
-    char *value  = arg->_value;
-
-    for (int i = 0; i < arg->volume; i++) {
+void *c_consB(void *multi_arg) {
+    char value;
+    for (int i = 0; i < info[1]; i++) {
         pthread_mutex_lock(&mutex);
-        while (count == 0 || buffer[use_ptr] != 'B')
+        while (balance == 0 || buffer[front] != 'B')
             pthread_cond_wait(&fill, &mutex);
-        get(value);
-        printf("B: %c \n", *value);
+        get(&value);
+        printf("B: %c \n", value);
         pthread_cond_signal(&empty);
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
-void *consumer_C(void *multi_arg) {
-    DA *arg = (DA *)multi_arg;
-    char *value  = arg->_value;
-
-    for (int i = 0; i < arg->volume; i++) {
+void *c_consC(void *multi_arg) {
+    char value;
+    for (int i = 0; i < info[2]; i++) {
         pthread_mutex_lock(&mutex);
-        while (count == 0 || buffer[use_ptr] != 'C')
+        while (balance == 0 || buffer[front] != 'C')
             pthread_cond_wait(&fill, &mutex);
-        get(value);
-        printf("C: %c \n", *value);
+        get(&value);
+        printf("C: %c \n", value);
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+void *c_consD(void *multi_arg) {
+    char value;
+    for (int i = 0; i < info[3]; i++) {
+        pthread_mutex_lock(&mutex);
+        while (balance == 0 || buffer[front] != 'D')
+            pthread_cond_wait(&fill, &mutex);
+        get(&value);
+        printf("D: %c \n", value);
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+void *c_consE(void *multi_arg) {
+    char value;
+    for (int i = 0; i < info[4]; i++) {
+        pthread_mutex_lock(&mutex);
+        while (balance == 0 || buffer[front] != 'E')
+            pthread_cond_wait(&fill, &mutex);
+        get(&value);
+        printf("E: %c \n", value);
         pthread_cond_signal(&empty);
         pthread_mutex_unlock(&mutex);
     }
@@ -99,42 +122,31 @@ void *consumer_C(void *multi_arg) {
 }
 
 void printQueue() {
-    for (int i = 0; i < count; i++) {
-        int index = (use_ptr + i) % MAX;
+    for (int i = 0; i < balance; i++) {
+        int index = (front + i) % LINE_SIZE;
         printf("%c ", buffer[index]);
     }
     printf("\n");
 }
 
 int main() {
-    pthread_t p,c1,c2,c3;
+    pthread_t p,c1,c2,c3,c4,c5;
 
-    char input[90] = {'A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B','A','A','B','C','A','C','C','B','B'};
-
-    DA dequeue_arg1;
-    char pop_value;
-	dequeue_arg1._value = &pop_value;
-    dequeue_arg1.volume = 30;
-
-    DA dequeue_arg2;
-	dequeue_arg2._value = &pop_value;
-    dequeue_arg2.volume = 30;
-
-    DA dequeue_arg3;
-	dequeue_arg3._value = &pop_value;
-    dequeue_arg3.volume = 30;
-
-    pthread_create(&p, NULL, producer,&input);
-    pthread_create(&c2, NULL, consumer_B,&dequeue_arg2);
-    pthread_create(&c1, NULL, consumer_A,&dequeue_arg1);
-    pthread_create(&c3, NULL, consumer_C,&dequeue_arg3);
+    pthread_create(&p, NULL, c_prod,NULL);
+    pthread_create(&c1, NULL, c_consA,NULL);
+    pthread_create(&c2, NULL, c_consB,NULL);
+    pthread_create(&c3, NULL, c_consC,NULL);
+    pthread_create(&c4, NULL, c_consD,NULL);
+    pthread_create(&c5, NULL, c_consE,NULL);
 
     pthread_join(p,NULL);
     pthread_join(c1,NULL);
     pthread_join(c2,NULL);
     pthread_join(c3,NULL);
+    pthread_join(c4,NULL);
+    pthread_join(c5,NULL);
 
     printQueue();
-    // pthread_join(c3,NULL);
+    printf("%d %d", pnum, cnum);
     printf("\n");
 }
